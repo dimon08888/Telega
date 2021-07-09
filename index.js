979f871eb6433dc7bba46c8bd76f447b3fbe1dd9
dimon08888 <dimon08888@mail.ru>
2021-07-09 14:34:38 +0300

    add online highlight, sort by name

--- index.js
+++ index.js
@@ -8,11 +8,27 @@ const contacts = [
     status: "Online",
   },
   {
+    name: "Alice Alison",
+    status: "Online",
+  },
+  {
+    name: "Alice Bobson",
+    status: "Online",
+  },
+  {
     name: "Bob Martin",
     status: "Last seen just now",
   },
   {
+    name: "Denis Albus",
+    status: "Online",
+  },
+  {
     name: "Gary Vee",
+    status: "Last seen just now",
+  },
+  {
+    name: "Denis Kruger",
     status: "Last seen just now",
   },
   {
@@ -24,12 +40,11 @@ const contacts = [
     status: "Last seen 7 hours ago",
   },
   {
-    name: "Arnold Schwarzenegger ",
+    name: "Arnold Schwarzenegger",
     status: "Last seen 10 minutes ago",
   },
 ];
 
-// TODO: if user status is online -> highlight it with differnet color.
 function renderContact(contact) {
   return `
     <li class="contact">
@@ -37,14 +52,77 @@ function renderContact(contact) {
       <div>
         <b>${contact.name}</b>
         <br>
-        <small>${contact.status}</small>
+        <small ${
+          contact.status === "Online"
+            ? 'style="color: var(--color-active)"'
+            : ""
+        }>${contact.status}</small>
       </div>
     </li>
-    `;
+  `;
 }
 
-const contactsList = document.querySelector(".contacts");
+// TODO(*): sort exactly like in telegram.
+function sort(contacts) {
+  function compare(contact1, contact2) {
+    const name1 = contact1.name.toLowerCase();
+    const name2 = contact2.name.toLowerCase();
 
-for (let contact of contacts) {
-  contactsList.innerHTML += renderContact(contact);
+    const [firstName1, lastName1] = name1.split(/\s+/);
+    const [firstName2, lastName2] = name2.split(/\s+/);
+
+    if (firstName1 < firstName2) {
+      return -1;
+    }
+
+    if (firstName1 > firstName2) {
+      return 1;
+    }
+
+    if (lastName1 < lastName2) {
+      return -1;
+    }
+
+    if (lastName1 > lastName2) {
+      return 1;
+    }
+
+    return 0;
+  }
+  return contacts.sort(compare);
 }
+
+function render() {
+  const contactsList = document.querySelector(".contacts");
+  sort(contacts);
+
+  const sortBy = document.querySelector("#sort-by");
+  sortBy.addEventListener("change", () => {
+    if (sortBy.value === "status") {
+      // TODO:
+      // sort by status...
+      console.log("updating ui"); // update ui
+    } else if (sortBy.value === "name") {
+      // TODO:
+      sort(contacts);
+      console.log("updating ui");
+    }
+  });
+
+  for (let contact of contacts) {
+    contactsList.innerHTML += renderContact(contact);
+  }
+
+  const buttons = document.querySelectorAll(".transparent-btn");
+
+  for (const button of buttons) {
+    button.addEventListener("click", () => {
+      for (const otherButton of buttons) {
+        otherButton.classList.remove("active");
+      }
+      button.classList.add("active");
+    });
+  }
+}
+
+render();
